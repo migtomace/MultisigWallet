5a7db8a89a2bdcab34d5f59bfa4b46ebc14509c3
Miguel Acevedo <migtomace@gmail.com>
2021-05-26 06:19:29 -0700

    Initial commit

---
+++ contracts/Wallet.sol
@@ -0,0 +1,80 @@
+pragma solidity ^0.6.0;
+pragma experimental ABIEncoderV2;
+
+contract Wallet {
+
+    //array of approvers
+    address[] public approvers;
+    //min number of approvers
+    uint public quorum;
+
+    //Transfer object
+    struct Transfer {
+        uint id;
+        uint amount;
+        address payable to;
+        uint approvals;
+        bool sent;
+    }
+    //Array of transfers
+    Transfer[] public transfers;
+    //map of approvals
+    mapping(address => mapping(uint => bool)) public approvals;
+
+
+    //constructor
+    //list of approvers and quorum
+    constructor(address[] _approvers, uint _quorum) public {
+        approvers = _approvers;
+        quorum = _quorum;
+    }
+
+    //getter functions
+    function getApprovers() external view returns(address[] memory){
+        return approvers;
+    }
+    function getTransfers() external view returns(Transfer[] memory){
+        return transfers;
+    }
+
+
+    function createTransfer(uint amount, address payable to) external onlyApprover {
+        transfers[nextId] = Transfer(
+            transfers.length,
+            amount,
+            to,
+            0,
+            false
+        );
+    }
+
+    function approveTransfer(uint id) external onlyApprover {
+        require(transfers[id].sent == false, 'transfer has already been sent.');
+        require(approvals[msg.sender][id] == false, 'cannot approve transfer twice.');
+
+        approvals[msg.sender][id] = true;
+        transfer[id].approvals++;
+
+        if(transfers[id].approvals >= quorum){
+            transfers[id].sent = true;
+            address payable to = transfers[id].to;
+            uint amount = transfers[id].amount;
+            to.transfer(amount);
+        }
+    }
+
+    receive() external payable {}
+
+    modifier onlyApprover() {
+        bool allowed = false;
+        for(uint i = 0; i < approvers.length; i++){
+            if(approvers[i] == msg.sender) {
+                allowed = true;
+            }
+            require(allowed == true, 'only approver allowed');
+            _;
+        }
+    }
+
+
+}
